b38d3739c40ea9628ca02aeb4933ec9fdee2c593
Ahmad Omid Saifi <132646206+AhmadOmidSaifi@users.noreply.github.com>
2025-06-17 21:37:18 -0700

    Update snake.py

--- snake.py
+++ snake.py
@@ -21,7 +21,7 @@ class Snake():
 
     def add_segment(self, position):
         new_segment = Turtle("turtle")
-        new_segment.color("black")
+        new_segment.color("blue")
         new_segment.penup()
         new_segment.goto(position)
         self.segment.append(new_segment)
